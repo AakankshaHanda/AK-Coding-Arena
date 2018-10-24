083955f254829cafc13cc894ef3091cbfca51892
AakankshaHanda <37653412+AakankshaHanda@users.noreply.github.com>
2018-10-24 22:27:05 +0530

    added new programs

---
+++ TriangularNumbers.cpp
@@ -0,0 +1,14 @@
+//Traingular numbers.
+//x(n) = n(n+1)/2
+#include<iostream>
+using namespace std;
+int main(){
+	int num=0;
+	 cout<<"enter number";
+	 cin>>num;
+	 num= (num*(num+1))/2;
+	 
+	 
+	 cout<<"number of dots are "<<num;
+}
+
--- reverseFirstAndLast.cpp
+++ reverseFirstAndLast.cpp
@@ -1,4 +1,33 @@
-//WAP to reverse first and last digit of a number.
-1023 - 3021
+//WAP to reverse the first and last digit of a number where a func getDigitCount() and reverse() are two functions with integer return type.
+#include<iostream>
+#include<math.h>
+using namespace std;
+int getDigitCount(int);
+int reverse(int);
+int main(){
+	int num,val,nodig;
+	cout<<"enter the number";
+	cin>>num;
+	cout<<reverse(num);
+	return 0;
+}
+int getDigitCount(int num){
+	int nod;
+	while(num>0){
+		nod++;
+		num=num/10;
+	}
+	return nod;
+}
 
-Log 10 (1023) - 3
+int reverse(int originalNumber){
+	int noOfDigits = getDigitCount(originalNumber);
+	int last=originalNumber%10;
+	int first=originalNumber/(pow(10.0,(double)noOfDigits-1));
+	double divisor = pow(10.0,(double)noOfDigits-1);
+	int middleNumber=originalNumber%(int)divisor;	 
+	middleNumber-=last;
+	int newNumber = (last*(pow(10,noOfDigits-1)))+middleNumber+first;
+	return newNumber;
+}
+
